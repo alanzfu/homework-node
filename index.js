fcab2ae6a39a8b42ad8b5105f883af60386cc35b
Alan Fu <alanzfu@gmail.com>
2017-04-15 15:51:11 -0700

    creates skeleton of download funcs

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 'use strict'
+const async = require('async');
+const fs = require('fs');
+const debug = require('debug')('homework-node:index:');
 
 module.exports = downloadPackages
 
+
+//entry point for package downloader
 function downloadPackages (count, callback) {
+  async.series([
+    findTopNumPackages,
+    downloadPackages
+  ], (err, result) => {
+    if (err) return callback(err);
+    debug(`Succesfully downloaded ${count} packages`);
+    callback();
+  });
+}
+
+//finds top packages from NPM
+function findTopNumPackages (count, callback) {
 
 }
+
+//downloads a single package
+function downloadPackage (packageUrl, callback) {
+
+}
+
+/*
+  find (x) number of packages
+  //download package
+*/
